d700bb4775ae8e2c7729485b2b2b10d7dd00b2d6
Amanda Matar <amandamatar@gmail.com>
2023-11-09 09:55:51 -0300

    projeto

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>30 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis in maiores aliquid, doloremque earum totam nemo itaque accusamus aspernatur dolor facilis sint repudiandae natus voluptas autem incidunt quaerat. Quas, recusandae.</p>
+  </div>
+</template>
+
+<style>
+  .about {
+    max-width: 300px;
+    margin: 10px auto;
+  }
+</style>
